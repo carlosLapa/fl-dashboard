b076adbc8a49d76e350498b0ec1138e1bb2f81be
carlosLapa <carlosferreiralapa@gmail.com>
2024-05-16 17:45:01 +0100

    User entity and resource, created, Postman project collection created

---
+++ backend/src/main/java/com/fl/dashboard/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.fl.dashboard.resources;
+
+import com.fl.dashboard.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    // refaz tudo hoje!
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = new ArrayList<>();
+        list.add(new User(1L, "Carlos", "Lapa", "carlos@gmail.com", "1234"));
+        list.add(new User(2L, "José", "Lapa", "jose@gmail.com", "1234"));
+        return ResponseEntity.ok().body(list);
+    }
+
+}
