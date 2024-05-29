192deafca13ebde84ca7872ecc1c2f2ade3e5a5b
carlosLapa <carlosferreiralapa@gmail.com>
2024-05-29 17:39:10 +0100

    Navbar visual modifications

--- frontweb/src/components/Navbar/indexNavbarRB.tsx
+++ frontweb/src/components/Navbar/indexNavbarRB.tsx
@@ -10,26 +10,33 @@ function NavScrollExample() {
   return (
     <Navbar expand="lg" className="bg-dark navbar-dark">
       <Container fluid>
-        <Navbar.Brand href="#" className="text-light">
+        <Navbar.Brand href="#" className="text-light me-5">
           Ferreira Lapa
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0 text-light"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1" className="text-light">
+        <Navbar.Collapse id="navbarScroll" className="justify-content-between">
+          <div className="d-flex me-3">
+            <Form className="d-flex me-auto">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-3"
+                aria-label="Search"
+              />
+              <Button variant="outline-light">Search</Button>
+            </Form>
+          </div>
+          <Nav className="my-2 my-lg-0" navbarScroll>
+            <Nav.Link href="#action1" className="text-light me-5">
               Home
             </Nav.Link>
-            <Nav.Link href="#action2" className="text-light">
+            <Nav.Link href="#action2" className="text-light me-5">
               Link
             </Nav.Link>
             <NavDropdown
               title="Dropdown"
               id="navbarScrollingDropdown"
-              className="text-light"
+              className="text-light me-5"
             >
               <NavDropdown.Item href="#action3" className="text-dark">
                 Action
@@ -42,19 +49,14 @@ function NavScrollExample() {
                 Something else here
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled className="text-light">
-              Disabled
+            <Nav.Link className="me-3">
+              <img
+                src="https://via.placeholder.com/32"
+                alt="User Avatar"
+                className="rounded-circle"
+              />
             </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-light">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
