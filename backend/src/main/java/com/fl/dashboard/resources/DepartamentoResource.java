742537f8480e7cfb68cc9273824bb1cd70f0521b
carlosLapa <carlosferreiralapa@gmail.com>
2024-06-13 16:31:11 +0100

    Departamento resource and service added

--- backend/src/main/java/com/fl/dashboard/resources/DepartamentoResource.java
+++ backend/src/main/java/com/fl/dashboard/resources/DepartamentoResource.java
@@ -2,12 +2,13 @@ package com.fl.dashboard.resources;
 
 import com.fl.dashboard.dto.DepartamentoDTO;
 import com.fl.dashboard.services.DepartamentoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +22,32 @@ public class DepartamentoResource {
     public ResponseEntity<List<DepartamentoDTO>> findAll() {
         List<DepartamentoDTO> list = departamentoService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartamentoDTO> findById(@PathVariable Long id) {
+        DepartamentoDTO departamentoDTO = departamentoService.findById(id);
+        return ResponseEntity.ok().body(departamentoDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<DepartamentoDTO> insert(@Valid @RequestBody DepartamentoDTO departamentoDTO) {
+        DepartamentoDTO newDto = departamentoService.insert(departamentoDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<DepartamentoDTO> update(@PathVariable Long id, @Valid @RequestBody DepartamentoDTO dto) {
+        DepartamentoDTO newDto = departamentoService.update(id, dto);
+        return ResponseEntity.ok().body(newDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        departamentoService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- backend/src/main/java/com/fl/dashboard/services/DepartamentoService.java
+++ backend/src/main/java/com/fl/dashboard/services/DepartamentoService.java
@@ -3,8 +3,13 @@ package com.fl.dashboard.services;
 import com.fl.dashboard.dto.DepartamentoDTO;
 import com.fl.dashboard.entities.Departamento;
 import com.fl.dashboard.repositories.DepartamentoRepository;
+import com.fl.dashboard.services.exceptions.DatabaseException;
+import com.fl.dashboard.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -20,6 +25,51 @@ public class DepartamentoService {
     public List<DepartamentoDTO> findAll() {
         List<Departamento> list = departamentoRepository.findAll();
         return list.stream().map(DepartamentoDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public DepartamentoDTO findById(Long id) {
+        Departamento entity = departamentoRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Tarefa com o id " + id + " não encontrado"));
+        return new DepartamentoDTO(entity);
+    }
+
+    @Transactional
+    public DepartamentoDTO insert(DepartamentoDTO departamentoDTO) {
+        Departamento entity = new Departamento();
+        copyDTOtoEntity(departamentoDTO, entity);
+        // entity = tarefaRepository.save(entity);
+        departamentoRepository.save(entity);
+        return new DepartamentoDTO(entity);
+    }
+
+    @Transactional
+    public DepartamentoDTO update(Long id, DepartamentoDTO departamentoDTO) {
+        try {
+            Departamento entity = departamentoRepository.getReferenceById(id);
+            copyDTOtoEntity(departamentoDTO, entity);
+            entity = departamentoRepository.save(entity);  // Ensure entity is saved after updating
+            return new DepartamentoDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id: " + id + " não foi encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!departamentoRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            departamentoRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(("Não permitido! Integridade da BD em causa"));
+        }
+    }
+
+    private void copyDTOtoEntity(DepartamentoDTO departamentoDTO, Departamento entity) {
+        entity.setDesignacao(departamentoDTO.getDesignacao());
+        entity.setDescricao(departamentoDTO.getDescricao());
     }
 
 }
