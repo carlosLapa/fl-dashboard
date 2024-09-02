4841c9a1826c351627cb0e77311e375d602a93a2
carlosLapa <carlosferreiralapa@gmail.com>
2024-09-02 15:38:02 +0100

    TarefaDTO Validations

--- backend/src/main/java/com/fl/dashboard/dto/TarefaDTO.java
+++ backend/src/main/java/com/fl/dashboard/dto/TarefaDTO.java
@@ -2,6 +2,9 @@ package com.fl.dashboard.dto;
 
 import com.fl.dashboard.entities.Tarefa;
 import com.fl.dashboard.enums.TarefaStatus;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,10 +17,19 @@ import java.util.Date;
 public class TarefaDTO {
 
     private Long id;
+
+    @Size(min = 5, max = 200, message = "Designação deve conter de 5 a 50 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String descricao;
+
     private String prioridade;
+
+    @FutureOrPresent(message = "Data deve ser no presente ou futuro")
     private Date prazoEstimado;
+
+    @FutureOrPresent(message = "Data deve ser no presente ou futuro")
     private Date prazoReal;
+
     private TarefaStatus status;
 
     public TarefaDTO() {
