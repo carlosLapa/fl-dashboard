3504ef464b3d4573261e304838c5c3438d7cfa01
carlosLapa <carlosferreiralapa@gmail.com>
2024-11-25 10:44:33 +0000

    RedisConfig - for the future

---
+++ backend/src/main/java/com/fl/dashboard/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.fl.dashboard.config;
+
+/* SOMENTE PARA LEMBRAR DE CONFIGURAR O REDIS AQUANDO EM PRODUÇÃO
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
+
+*/
